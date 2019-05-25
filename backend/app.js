62eb19c8e5f6fd31b5941e043137017ff767af87
birame <birametgod95@gmail.com>
2019-05-25 18:04:57 +0200

    add endpoint to signup

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
-
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('regar mon middleware');
-  next();
-});
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.send('hello from express');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-Width, Content-Type,Accept,Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PATCH,DELETE,OPTIONS,PUT'
+  );
+  next();
+});
+app.use('/api/user', userRoutes);
+app.use('/', (req, res) => {
+  return res.status(200).json({
+    testing: 'good'
+  });
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,9 @@
-const server = require('../server');
+const app = require('../backend/app');
 const request = require('supertest');
 
-// close the server after each test
-afterEach(() => {
-  server.close();
-});
-describe('routes: index', () => {
-  it('should respond as expected', async () => {
-    const response = await request(server).get('/');
-    expect(response.status).toEqual(200);
-    expect(response.type).toEqual('text/html');
+describe('testing /', () => {
+  it('should respond thats good', async () => {
+    const res = await request(app).get('/');
+    expect(res.status).toBe(200);
   });
 });
