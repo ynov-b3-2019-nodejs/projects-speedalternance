70beb253dd7300aa951878a88dc01988021c2abd
Brice MICHALSKI <brice.michalski@icloud.com>
2019-05-27 10:03:09 +0200

    Header component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
+Vue.config.$http = Axios
+Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
