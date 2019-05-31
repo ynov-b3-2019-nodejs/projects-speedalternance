98ad1ddc3b45d8074967500ddb997b25abcad4a4
Brice MICHALSKI <brice.michalski@icloud.com>
2019-05-31 18:20:56 +0200

    async jest test

--- test/server.test.js
+++ test/server.test.js
@@ -2,16 +2,18 @@ const app = require('../backend/app');
 const request = require('supertest');
 
 describe('testing /', () => {
-  it('should respond thats good', async () => {
+  it('should respond thats good', async (done) => {
     const res = await request(app).get('/');
     expect(res.status).toBe(200);
-    expect(JSON.stringify(res.body)).toBe('{"testing":"good"}')
+    expect(JSON.stringify(res.body)).toBe('{"testing":"good"}');
+    done();
   });
 });
 
 describe('testing /api/post', () => {
-  it('should respond thats good', async () => {
+  it('should respond thats good', async (done) => {
     const res = await request(app).get('/api/post');
     expect(res.status).toBe(200);
+    done();
   });
 });
