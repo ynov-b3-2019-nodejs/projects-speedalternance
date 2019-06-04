c5cb8c9fa8f00e75f16a859d39c1bdae9ad5b03e
birame <birametgod95@gmail.com>
2019-06-04 10:02:26 +0200

    testing

--- backend/controllers/chatRoom.js
+++ backend/controllers/chatRoom.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 4000);
+server.listen(4000);
 
 io.on('connection', () => {
   console.log('user connected');
