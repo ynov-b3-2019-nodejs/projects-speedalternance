722ba08286812e6dc99d3b3e64b8699c594b8fa7
birame <birametgod95@gmail.com>
2019-06-11 12:29:13 +0200

    change test

--- test/server.test.js
+++ test/server.test.js
@@ -1,16 +1,8 @@
 const app = require('../backend/app');
 const request = require('supertest');
 
-describe('testing /', () => {
-  it('should respond thats good', async (done) => {
-    const res = await request(app).get('/');
-    expect(res.status).toBe(200);
-    done();
-  });
-});
-
 describe('testing /api/post', () => {
-  it('should respond thats good', async (done) => {
+  it('should respond thats good', async done => {
     const res = await request(app).get('/api/post');
     expect(res.status).toBe(200);
     done();
