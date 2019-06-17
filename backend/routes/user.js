abfc57a25d1df67cdaa1012e91dca2e94090906b
birame <birametgod95@gmail.com>
2019-06-17 21:44:34 +0200

    add new endpoint for get user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,4 +6,5 @@ const userControllers = require('../controllers/user');
 router.post('/signup', userControllers.signUp);
 router.post('/login', userControllers.login);
 router.get('/', userControllers.getUser);
+router.get('/:id', userControllers.getCurrentUser);
 module.exports = router;
