697cd54299b8ce70acc8d772ac0cd3010f585dce
birame <birametgod95@gmail.com>
2019-06-17 17:25:14 +0200

    get current user chat

--- backend/routes/chatRoom.js
+++ backend/routes/chatRoom.js
@@ -4,6 +4,7 @@ const checkAuth = require('../middleware/check-auth');
 const chatControllers = require('../controllers/chatRoom');
 
 router.get('/', chatControllers.getChat);
+router.get('/user', chatControllers.getCurrentUserChat);
 router.get('/:id', chatControllers.getCurrentChat);
 router.post('/', chatControllers.createChat);
 router.put('/:id', chatControllers.updateChat);
